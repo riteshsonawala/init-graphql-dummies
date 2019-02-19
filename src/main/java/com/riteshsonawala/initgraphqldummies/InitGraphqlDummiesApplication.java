3ca15b9e5bf71d8d9c4007aa332b1b10bae9616f
ritesh <ritesh.sonawala@gmail.com>
2019-02-19 21:39:36 +0000

    init

---
+++ src/main/java/com/riteshsonawala/initgraphqldummies/InitGraphqlDummiesApplication.java
@@ -0,0 +1,13 @@
+package com.riteshsonawala.initgraphqldummies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitGraphqlDummiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitGraphqlDummiesApplication.class, args);
+	}
+
+}
